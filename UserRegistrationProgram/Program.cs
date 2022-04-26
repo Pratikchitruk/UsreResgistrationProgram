2a584de7f2d9e17cdec9fc388c0ade3bbe4624aa
PratikChitruk <pratik.chitruk@gmail.com>
2022-04-26 20:04:24 +0530

    [pratik] add program.cs

---
+++ UserRegistrationProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to User Registration Program and Input validation using RegEx \n");
+
+            //InputValidation validate = new InputValidation();
+        }
+    }
+}
